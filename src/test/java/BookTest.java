99fd706f0c3c47ceb55c7833490694e9647b429f
samina jadoon <saminakjadoon@gmail.com>
2026-05-23 22:34:42 +1000

    first commit

---
+++ src/test/java/BookTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void getTitle() {
+    }
+
+    @Test
+    void getQuantity() {
+    }
+
+    @Test
+    void getPrice() {
+    }
+
+    @Test
+    void getType() {
+    }
+}
